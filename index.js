805cde80720df37048e8e0976edfdfc285eadf07
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-11-05 07:33:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 
+const { nextISSTimesForMyLocation } = require('./iss');
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(passTimes);
+});
+
+
+
+
+
+// fetchCoordsByIP("76.67.182.110",(error,data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log("callback Data",data);
+// })
+
+
+
+
+
+//    fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
